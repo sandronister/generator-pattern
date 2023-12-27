4fbcb062c9447d12c5482fb213e797fa3c837b2b
Julio Sandroni <sandronister@gmail.com>
2023-12-27 15:33:56 -0300

    chore:Get Best

--- pkg/request/main.go
+++ pkg/request/main.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"net/http"
 	"regexp"
+	"time"
 )
 
 func GetTitle(urls ...string) <-chan string {
@@ -27,4 +28,25 @@ func GetTitle(urls ...string) <-chan string {
 
 	}
 	return c
+}
+
+func GetBest(url1, url2, url3, url4 string) string {
+	c1 := GetTitle(url1)
+	c2 := GetTitle(url2)
+	c3 := GetTitle(url3)
+	c4 := GetTitle(url4)
+
+	select {
+	case t1 := <-c1:
+		return t1
+	case t2 := <-c2:
+		return t2
+	case t3 := <-c3:
+		return t3
+	case t4 := <-c4:
+		return t4
+	case <-time.After(1000 * time.Millisecond):
+		return "Timeout"
+
+	}
 }
